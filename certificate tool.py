fba1e4ee6f41aa63f183c6eb5ce5f95cc115e845
Ori Levi <101058837+orilevicyber@users.noreply.github.com>
2022-03-08 21:40:20 +0200

    Update certificate tool.py

--- certificate tool.py
+++ certificate tool.py
@@ -2,43 +2,43 @@ import pyfiglet
 import os
 from ppadb.client import Client as AdbClient
 import time
+import fileinput
+import re
 
-print(pyfiglet.figlet_format("Certificate tool", font ="cybermedium"))
+print(pyfiglet.figlet_format("Certificate tool", font="cybermedium"))
 
-try:
- os.system("adb devices > devices.txt")
 
- print("Insert path Der")
- der = input()
- os.system("openssl x509 -inform der -in "+der+" -out mobile.pem")
- os.system("openssl x509 -inform PEM -subject_hash_old -in mobile.pem > mobile.txt")
+print("Insert path Der")
+der = input()
+os.system("openssl x509 -inform der -in " + der + " -out mobile.pem")
+os.system("openssl x509 -inform PEM -subject_hash_old -in mobile.pem > mobile.txt")
+os.system("adb devices > devices.txt")
+fd = open("mobile.txt", "r")
+lines = fd.readlines()[0:1]
+for line in lines:
+    nameHash = line.rstrip()
+    fd.close()
+    crt = nameHash + ".0"
+    os.system("move mobile.pem " + crt)
+    os.system("adb push " + crt + " /sdcard/")
 
- fd = open("mobile.txt","r")
- lines = fd.readlines()[0:1]
- for line in lines:
-  nameHash = line.rstrip()
- fd.close()
- crt = nameHash+".0"
- os.system("move mobile.pem "+crt)
- os.system("adb push " +crt+ " /sdcard/")
+    fdd = open("devices.txt", "r")
+    lines = fdd.readlines()[1:2]
+    for line in lines:
+        data = line.rstrip()
 
- fdd = open("devices.txt","r")
- lines = fdd.readlines()[1:2]
- for line in lines:
-  data = line.rstrip()
- ipPort = data[0:19]
- ip = data[0:14]
- client = AdbClient(host="127.0.0.1", port=5037)
- client.remote_connect(ip, 5555)
- device = client.device(str(ipPort))
- time.sleep(2)
- response1 = device.remount()
- time.sleep(2)
- response2 = device.shell('mv /sdcard/9a5ba575.0 /system/etc/security/cacerts/')
- time.sleep(2)
- response3 = device.shell('chmod 644 /system/etc/security/cacerts/9a5ba575.0')
- response4 = device.reboot()
 
-except:
-  print("Connection Error")
-  
+    for line in fileinput.input("devices.txt"):
+            ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', line)
+            if ip:
+                for i in ip:
+                    client = AdbClient(host="127.0.0.1", port=5037)
+                    client.remote_connect(i, 5555)
+                    device = client.device(str(i + ":" + "5555"))
+                    time.sleep(2)
+                    response1 = device.remount()
+                    time.sleep(2)
+                    response2 = device.shell('mv /sdcard/9a5ba575.0 /system/etc/security/cacerts/')
+                    time.sleep(2)
+                    response3 = device.shell('chmod 644 /system/etc/security/cacerts/9a5ba575.0')
+                    response4 = device.reboot()
